1b20fee06c3f1208e1614d16840036537bf754a1
Frederic Bourouliou <bourouliou@gmail.com>
2025-01-26 16:45:55 +0100

    Update files 101

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -20,13 +20,35 @@ def lazy_matrix_mul(m_a, m_b):
         ValueError: If matrices can't be multiplied or are empty
         TypeError: If inputs are not valid matrices
     """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+
     try:
-        return np.matmul(m_a, m_b).tolist()
-    except ValueError as e:
-        if "shapes" in str(e):
-            raise ValueError("m_a and m_b can't be multiplied")
-        raise ValueError("empty matrices")
-    except TypeError as e:
-        if "list" in str(e):
-            raise TypeError("m_a must be a list or m_b must be a list")
+        np_a = np.array(m_a)
+        np_b = np.array(m_b)
+        result = np.matmul(np_a, np_b)
+        return result.tolist()
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
+    except TypeError:
+        if not all(isinstance(x, (int, float)) for row in m_a for x in row):
+            raise TypeError("m_a should contain only integers or floats")
+        if not all(isinstance(x, (int, float)) for row in m_b for x in row):
+            raise TypeError("m_b should contain only integers or floats")
+        if not all(len(row) == len(m_a[0]) for row in m_a):
+            raise TypeError("each row of m_a must be of the same size")
+        if not all(len(row) == len(m_b[0]) for row in m_b):
+            raise TypeError("each row of m_b must be of the same size")
         raise TypeError("invalid input") 
