5fb02b4161f4b981a43ec7fe503c1927c2f62880
Frederic Bourouliou <bourouliou@gmail.com>
2025-01-27 11:49:14 +0100

    update file 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -19,8 +19,13 @@ class Square:
 
     """
     def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def __setattr__(self, name, value):
         assert isinstance(value, int)
         super().__setattr__(name, value)
+   
