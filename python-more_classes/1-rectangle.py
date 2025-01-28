6fbbe8c37f45e4e2cf215ccc1216ec9af3216d81
Frederic Bourouliou <bourouliou@gmail.com>
2025-01-28 11:48:39 +0100

    update files 0 + 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+This module defines a Rectangle class.
+This is a simple class that represents a Rectangle shape.
+It serves as a basic implementation with no attributes or methods yet.
+"""
+class Rectangle:
+    """Class that defines a Rectangle.
+
+    This is an empty class that serves as a basic structure
+    for future implementations of Rectangle-related functionality.
+    """
+    def width(self):
+        """Initialize a new Rectangle instance.
+
+        Args:
+            size (int): The size of the Rectangle's sides.
+        """
+        def width(self, value):
+            self.__width = width
