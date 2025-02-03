1698f305409a6a9ca745ccf929c5f2ce4ea2f2db
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-03 16:11:12 +0100

    update file 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module for inherits_from method."""
+
+
+def inherits_from(obj, a_class):
+    """Check if object is an instance of a class that inherited from specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj to.
+
+    Returns:
+        True if obj is an instance of a class that inherited from a_class,
+        otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
