fb660891b7ab3f75b51763631ccb898af29926d5
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-03 14:18:37 +0100

    update file 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""Module for lookup method."""
+
+
 def lookup(obj):
-    return(list, obj)
+    """Returns list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to get attributes and methods from.
+
+    Returns:
+        List of attributes and methods.
+    """
+    return dir(obj)
