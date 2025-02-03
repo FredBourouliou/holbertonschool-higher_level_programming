246516432bba92b00f821e1761f87a4a5050fe12
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-03 16:47:35 +0100

    Update file 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Method to compute area.
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")
