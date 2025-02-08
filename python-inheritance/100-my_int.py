bb641ed4e6f7c42518b812de6a403a4e2623180c
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-08 20:31:00 +0100

    update file 100 + 101

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module for MyInt class."""
+
+
+class MyInt(int):
+    """MyInt class that inherits from int with inverted == and != operators."""
+
+    def __eq__(self, other):
+        """Override == operator with != behavior."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Override != operator with == behavior."""
+        return super().__eq__(other)
--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Module for add_attribute method."""
+
+
+def add_attribute(obj, name, value):
+    """Add a new attribute to an object if possible.
+
+    Args:
+        obj: The object to add an attribute to.
+        name: The name of the attribute to add.
+        value: The value of the attribute to add.
+
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
