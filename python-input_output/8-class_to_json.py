92bbc8abeea452caa930a91136873e6ce4a2e7c2
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-11 17:21:19 +0100

    update file 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-"""Module for converting class to JSON"""
+"""
+Module for converting class to JSON
+Contains function that returns dictionary description of object
+"""
 
 
 def class_to_json(obj):
-    """Function that returns the dictionary description with simple data structure
-    for JSON serialization of an object"""
+    """Returns dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args:
+        obj: instance of a Class
+
+    Returns:
+        dict: dictionary description of object
+    """
     return obj.__dict__
