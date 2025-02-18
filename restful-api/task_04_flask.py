d57bda1d36f362242647db530aebc8ab0ccd08fc
Frederic Bourouliou <bourouliou@gmail.com>
2025-02-18 18:18:11 +0100

    update task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -20,24 +20,20 @@ users = {
     }
 }
 
-
 @app.route('/')
 def home():
     """Root endpoint"""
     return "Welcome to the Flask API!"
-
 
 @app.route('/data')
 def get_data():
     """Return list of all usernames"""
     return jsonify(list(users.keys()))
 
-
 @app.route('/status')
 def get_status():
     """Return API status"""
     return "OK"
-
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -45,7 +41,6 @@ def get_user(username):
     if username in users:
         return jsonify(users[username])
     return jsonify({"error": "User not found"}), 404
-
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
@@ -69,17 +64,17 @@ def add_user():
         "user": new_user
     }), 201
 
-
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     """Return JSON instead of HTML for HTTP errors."""
     response = e.get_response()
     response.data = jsonify({
-        "error": e.description
-    })
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    }).data
     response.content_type = "application/json"
     return response
-
 
 if __name__ == '__main__':
     app.run(port=5000)
