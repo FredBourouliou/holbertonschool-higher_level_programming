5470bdd7e85e348345aa9a56db88bb37600f89f8
Frederic Bourouliou <bourouliou@gmail.com>
2025-03-05 19:06:56 +0100

    add file 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query all City objects and join with State objects
+    query = session.query(City, State).join(State).order_by(City.id).all()
+
+    # Display the results
+    for city, state in query:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Close the session
+    session.close()
