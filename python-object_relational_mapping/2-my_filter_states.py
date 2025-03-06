be1f0a162c38ef03b83447204d9159656972f8bf
Frederic Bourouliou <bourouliou@gmail.com>
2025-03-06 16:34:39 +0100

    update files 1 + 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     # Check if all required arguments are provided
     if len(sys.argv) != 5:
         print("Usage: {} <mysql username> <mysql password> <database name> "
-            "<state name>".format(sys.argv[0]))
+             "<state name>".format(sys.argv[0]))
         sys.exit(1)
 
     # Get command line arguments
